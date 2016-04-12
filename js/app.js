0bd938bbef3e139b6955832c162b777bac1bbf7f
cjjujubee <connie.jew@gmail.com>
2016-04-12 14:35:07 -0700

    Add ability to check and uncheck items and remove newly added items on list

--- js/app.js
+++ js/app.js
@@ -2,10 +2,19 @@ $(document).ready(function() {
 	$('#userInput').submit(function(event) {
 		event.preventDefault();
 		var something = $('#inputBox').val();
-		console.log(something);
-		$('.allItems ul').append("<li> <input type='checkbox'>" + something + "<button type='submit'><img src='assets/delete.png'/></button></li>");
+		$('.allItems ul').append("<li class='currentItem'> <button><img class='checked' src='assets/check.png'/></button>" + something + "<button type='submit'><img src='assets/delete.png'/ class='trash'></button></li>");
 	});
-	$('ul').on('click', '.trash', function(event){
-		$(this).closest('li').remove();
-	});
+
 });
+assignlisteners();
+function assignlisteners() {
+	$('ul').on('click', '.trash', function(event){
+		console.log('trash clicked')
+		$(this).closest('li').remove();
+	});	
+	$('ul').on('click', '.checked', function(event) {
+		$(this).closest('li').toggleClass('linethrough');
+		console.log('check clicked')
+		console.log(event.currentTarget)
+	});
+}
