b2e862181ccccb2ea07a8c1213a0e4d8e2d07ada
cjjujubee <connie.jew@gmail.com>
2016-04-12 16:32:05 -0700

    Change icons and font

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('#userInput').submit(function(event) {
 		event.preventDefault();
 		var something = $('#inputBox').val();
-		$('.allItems ul').append("<li class='currentItem'> <img class='checked' src='assets/check.png'/>" + " " + something + " " + "<button type='submit'><img src='assets/delete.png'/ class='trash'></button></li>");
+		$('.allItems ul').append("<li class='currentItem'> <img class='checked' src='assets/check.png'/>" + " " + something + " " + "<img src='assets/delete.png'/ class='trash'></li>");
 		$('#userInput')[0].reset();
 	});
 
@@ -10,12 +10,11 @@ $(document).ready(function() {
 assignlisteners();
 function assignlisteners() {
 	$('ul').on('click', '.trash', function(event){
-		console.log('trash clicked')
+		console.log('trash clicked');
 		$(this).closest('li').remove();
 	});	
 	$('ul').on('click', '.checked', function(event) {
 		$(this).closest('li').toggleClass('linethrough');
-		console.log('check clicked')
-		console.log(event.currentTarget)
+		console.log('check clicked');
 	});
 }
