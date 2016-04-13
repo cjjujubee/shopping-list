ddff5edb6266e9179cad2f1b0c49173f06f269ff
cjjujubee <connie.jew@gmail.com>
2016-04-12 20:42:27 -0700

    Removes ability to append empty textbox input and makes design changes. Adds comments for jQuery code.

--- js/app.js
+++ js/app.js
@@ -2,11 +2,21 @@ $(document).ready(function() {
 	$('#userInput').submit(function(event) {
 		event.preventDefault();
 		var something = $('#inputBox').val();
-		$('.allItems ul').append("<li class='currentItem'> <img class='checked' src='assets/check.png'/>" + " " + something + " " + "<img src='assets/delete.png'/ class='trash'></li>");
+		// if user inputs nothing and hits enter, alert will pop up and nothing is appended
+		if (something === "") {
+			alert('Please enter an item!')
+		}
+		//otherwise, text is appended as a li to the shopping list
+		else {
+			$('.allItems ul').append("<li class='currentItem'> <img class='checked' src='assets/check.png'/>" + " " + something + " " + "<img src='assets/delete.png'/ class='trash'></li>");	
+		}
+		// resets the textbox after item is entered
 		$('#userInput')[0].reset();
 	});
 
 });
+
+//functions to delete and strikethrough code when checkbox and trashcan are clicked
 assignlisteners();
 function assignlisteners() {
 	$('ul').on('click', '.trash', function(event){
